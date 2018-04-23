5f4e6ed9cf617587bb4f355d3c6f0ef8f7f79ecb
KseniaZhe <zhel0002@algonquinlive.com>
2018-04-23 17:50:21 -0400

    Change paths on product page and on details page to get the missing images

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/KseZhe/Dropbox/algonquin/Graphic Design Program/Semester 4/1. Web IV/week3/ecommerce-pattern-library
- * @version 1524519898090
+ * @version 1524520191656
  */
-const patternManifest_1524519898089 = {
+const patternManifest_1524520191656 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -1188,5 +1188,5 @@ const patternManifest_1524519898089 = {
   }
 };
 
-patternBotIncludes(patternManifest_1524519898089);
+patternBotIncludes(patternManifest_1524520191656);
 }());
